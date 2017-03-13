0964c602a06c3cec946bfdfefa1b9504a5b24b83
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-03-13 03:36:31 -0300

    feat: send device location to server

--- android/app/src/main/java/io/perenecabuto/catchcatch/MainActivity.java
+++ android/app/src/main/java/io/perenecabuto/catchcatch/MainActivity.java
@@ -1,23 +1,27 @@
 package io.perenecabuto.catchcatch;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.view.Window;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.location.LocationManager.NETWORK_PROVIDER;
@@ -33,6 +37,7 @@ public class MainActivity extends Activity {
 
     private MapFragment mapFragment;
     private GoogleMap map;
+    private ConnectionManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +51,33 @@ public class MainActivity extends Activity {
             // TODO OnMapCreate get features around and plot them
             m.setOnCameraMoveListener(() -> {
                 // TODO OnCameraChange get features around and plot them
-                Log.d(TAG, "position: " + m.getCameraPosition().target + "zoom: " + m.getCameraPosition().zoom);
+                // Log.d(TAG, "position: " + m.getCameraPosition().target + "zoom: " + m.getCameraPosition().zoom);
             });
         });
+
+        setupConnection();
 
         if (checkCallingOrSelfPermission(ACCESS_FINE_LOCATION) == PERMISSION_GRANTED) {
             setupLocation();
         } else {
             requestPermission();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        manager.disconnect();
+    }
+
+    private void setupConnection() {
+        try {
+            Socket socket = IO.socket("http://192.168.23.102:5000");
+            manager = new ConnectionManager(socket);
+            manager.connect();
+        } catch (URISyntaxException | ConnectionManager.NoConnectionException e) {
+            e.printStackTrace();
+            Log.e(TAG, e.getMessage(), e);
         }
     }
 
@@ -78,8 +102,13 @@ public class MainActivity extends Activity {
     private void setupLocation() {
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(provider, 0, 0, new LocationUpdateListener((Location l) -> {
-            // TODO send my location to server
             Log.d(TAG, "location updated");
+            try {
+                manager.sendPosition(l);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Error to send position", Toast.LENGTH_SHORT).show();
+            }
             focusOn(l);
         }));
 
