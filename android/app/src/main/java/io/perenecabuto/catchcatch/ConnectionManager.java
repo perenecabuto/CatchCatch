667542c6719f5353091092cd62c8aab639738122
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-03-13 03:34:40 -0300

    feat: bind socket.io events

--- android/app/src/main/java/io/perenecabuto/catchcatch/ConnectionManager.java
+++ android/app/src/main/java/io/perenecabuto/catchcatch/ConnectionManager.java
@@ -1,8 +1,47 @@
 package io.perenecabuto.catchcatch;
 
-/**
- * Created by bacamarte on 3/13/17.
- */
+
+import android.location.Location;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.util.Arrays;
+
+import io.socket.client.Socket;
 
 class ConnectionManager {
+
+    private static final String TAG = ConnectionManager.class.getName();
+    private Socket socket;
+
+    ConnectionManager(Socket socket) {
+        this.socket = socket;
+    }
+
+    void connect() throws URISyntaxException, NoConnectionException {
+        socket
+            .on(Socket.EVENT_CONNECT, args -> Log.d(TAG, "connect: " + Arrays.toString(args)))
+            .on("player:list", args -> Log.d(TAG, "player:list" + Arrays.toString(args)))
+            .on("player:updated", args -> Log.d(TAG, "player:updated" + Arrays.toString(args)))
+            .on("player:new", args -> Log.d(TAG, "player:new" + Arrays.toString(args)))
+            .on(Socket.EVENT_DISCONNECT, args -> Log.d(TAG, "disconnect: " + Arrays.toString(args)));
+
+        socket.connect();
+    }
+
+    void sendPosition(Location l) throws JSONException {
+        JSONObject coords = new JSONObject();
+        coords.put("x", l.getLatitude());
+        coords.put("y", l.getLongitude());
+        socket.emit("player:update", coords.toString());
+    }
+
+    class NoConnectionException extends Exception {
+        NoConnectionException(String msg) {
+            super(msg);
+        }
+    }
 }
