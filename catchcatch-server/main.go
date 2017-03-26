2c8ccee17ada00368768c3fd7a12c1e7647917b0
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-03-26 19:53:33 -0300

    fix: typo

--- catchcatch-server/main.go
+++ catchcatch-server/main.go
@@ -18,7 +18,7 @@ var (
 	tile38Addr     = flag.String("tile38-addr", "localhost:9851", "redis address")
 	maxConnections = flag.Int("tile38-connections", 100, "tile38 address")
 	port           = flag.Int("port", 8888, "server port")
-	webDir         = flag.Strng("web-dir", "../web", "web files dir")
+	webDir         = flag.String("web-dir", "../web", "web files dir")
 )
 
 func main() {
