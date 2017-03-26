ea1aecbbdc9e01ab17f6298d86b95192d3f66446
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-03-26 19:28:33 -0300

    refactor: rename redis refs to tile38

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: echo $PWD && ls && catchcatch-server -port 5000 -redis-addr 172.17.0.2:9851
+web: echo $PWD && ls && catchcatch-server -port 5000 -tile38-addr 172.17.0.2:9851
--- catchcatch-server/main.go
+++ catchcatch-server/main.go
@@ -15,25 +15,25 @@ import (
 )
 
 var (
-	redisAddress   = flag.String("redis-addr", "localhost:9851", "redis address")
-	maxConnections = flag.Int("redis-connections", 100, "redis address")
+	tile38Addr     = flag.String("tile38-addr", "localhost:9851", "redis address")
+	maxConnections = flag.Int("tile38-connections", 100, "tile38 address")
 	port           = flag.Int("port", 8888, "server port")
 )
 
 func main() {
 	flag.Parse()
 
-	client := mustRedisConnect()
+	client := mustConnectTile38()
 	service := &PlayerLocationService{client}
 	client.WrapProcess(func(oldProcess func(cmd redis.Cmder) error) func(cmd redis.Cmder) error {
 		return func(cmd redis.Cmder) error {
-			log.Println("REDIS DEBUG:", cmd.String())
+			log.Println("TILE38 DEBUG:", cmd.String())
 			return oldProcess(cmd)
 		}
 	})
 
 	go func() {
-		err := service.StreamGeofenceEvents(addr, func(msg string) {
+		err := service.StreamGeofenceEvents(*tile38Addr, func(msg string) {
 			log.Println("geofence:event", msg)
 		})
 		if err != nil {
@@ -56,21 +56,21 @@ func main() {
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
 
-func mustRedisConnect() *redis.Client {
-	client := redis.NewClient(&redis.Options{Addr: *redisAddress, PoolSize: 1000, DialTimeout: 1 * time.Second})
+func mustConnectTile38() *redis.Client {
+	client := redis.NewClient(&redis.Options{Addr: *tile38Addr, PoolSize: 1000, DialTimeout: 1 * time.Second})
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Kill, os.Interrupt)
 	go func() {
 		<-c
-		redisCleanUp(client)
+		tile38Cleanup(client)
 		os.Exit(0)
 	}()
 
 	return client
 }
 
-func redisCleanUp(conn *redis.Client) {
+func tile38Cleanup(conn *redis.Client) {
 	log.Println("Cleaning location DB...")
 	conn.FlushDb()
 	conn.Close()
