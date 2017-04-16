d3d9e262a1504d0ad5263cf9897797171bbb11f9
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-04-16 18:33:19 -0300

    fix: send to target player when looses

--- catchcatch-server/game.go
+++ catchcatch-server/game.go
@@ -159,6 +159,7 @@ func (g *Game) updateAndNofityPlayer(p *Player, sessions *SessionManager) {
 	dist := p.DistTo(g.targetPlayer)
 	if dist <= 20 {
 		log.Printf("game:%s:detect=winner:%s:dist:%f\n", g.ID, p.ID, dist)
+		sessions.Emit(g.targetPlayer.ID, "game:loose", g.ID)
 		delete(g.players, g.targetPlayer.ID)
 		sessions.Emit(p.ID, "game:target:reached", strconv.FormatFloat(dist, 'f', 0, 64))
 		g.Stop()
