2aad6fce72370ed0378b072d0a15d1a09b54dc9f
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-04-20 07:59:18 -0300

    refactor: update HomeActivity to use new EventHandlers

--- android/app/src/main/java/io/perenecabuto/catchcatch/HomeActivity.kt
+++ android/app/src/main/java/io/perenecabuto/catchcatch/HomeActivity.kt
@@ -2,10 +2,10 @@ package io.perenecabuto.catchcatch
 
 import android.location.Location
 import android.os.Bundle
-import android.os.Handler
 import io.nlopez.smartlocation.SmartLocation
 import io.nlopez.smartlocation.location.config.LocationAccuracy
 import io.nlopez.smartlocation.location.config.LocationParams
+import org.json.JSONObject
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import java.util.*
@@ -13,13 +13,15 @@ import java.util.*
 private val dialogsDelay: Long = 5000L
 
 
-class HomeActivity : ActivityWithLocationPermission(), PlayerEventListener.Handler {
-    private val TAG = HomeActivity::class.java.simpleName
+class HomeActivity : ActivityWithLocationPermission() {
 
     internal var player = Player("", 0.0, 0.0)
-    private var gameListener: GameEventListener? = null
-    private var playerListener: PlayerEventListener? = null
+    private var animator: PolygonAnimator? = null
     private var map: MapView? = null
+
+    private var radar: RadarEventHandler? = null
+    private var game: GameEventHandler? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +30,30 @@ class HomeActivity : ActivityWithLocationPermission(), PlayerEventListener.Handl
 
         map = OSMShortcuts.findMapById(this, R.id.home_activity_map)
 
-        val app = application as CatchCatch
-        gameListener = GameEventListener(app.socket!!, GameEventHandler(this, map!!))
-        gameListener!!.connect()
-        playerListener = PlayerEventListener(app.socket!!, this)
-        playerListener!!.connect()
-
         val conf = LocationParams.Builder().setAccuracy(LocationAccuracy.HIGH).build()
         SmartLocation.with(this).location().continuous().config(conf).start(this::onLocationUpdate)
 
-        val random = Random()
-        RankDialog(this, GameRank("CatchCatch", (0..10).map { PlayerRank("Player $it", random.nextInt()) })).show()
-        TransparentDialog(this, "welcome!").showWithTimeout(dialogsDelay)
+        val app = application as CatchCatch
+        radar = RadarEventHandler(app.socket!!, this)
+        radar!!.start()
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+
+        showMessage("welcome!")
+        showRank(GameRank("CatchCatch", (0..10).map { PlayerRank("Player $it", Random().nextInt()) }))
     }
 
     private fun onLocationUpdate(l: Location) {
         val point = GeoPoint(l.latitude, l.longitude)
         OSMShortcuts.focus(map!!, point)
         player.updateLocation(l)
-        playerListener!!.sendPosition(l)
-        OSMShortcuts.showMarkerOnMap(map!!, "me", point)
+        sendPosition(l)
+
+        if (animator == null || animator!!.running.not()) {
+            OSMShortcuts.showMarkerOnMap(map!!, "me", point)
+        }
     }
 
     override fun onResume() {
@@ -60,62 +66,42 @@ class HomeActivity : ActivityWithLocationPermission(), PlayerEventListener.Handl
         map!!.overlays.clear()
     }
 
-    override fun onRegistered(p: Player) = runOnUiThread {
-        player = p
-        TransparentDialog(this, "Connected as\n${p.id}").showWithTimeout(dialogsDelay)
+    fun showMessage(msg: String) = runOnUiThread {
+        TransparentDialog(this, msg).showWithTimeout(dialogsDelay)
     }
 
-    override fun onDisconnected() = runOnUiThread {
-        TransparentDialog(this, "Disconnected").showWithTimeout(5000)
+    fun showRank(rank: GameRank) = runOnUiThread {
+        RankDialog(this, rank).showWithTimeout(dialogsDelay)
     }
-}
 
-class GameEventHandler(val activity: HomeActivity, val map: MapView) : GameEventListener.Handler {
-    private val TAG = GameEventHandler::class.java.simpleName
-    private var info: GameInfo? = null
-    private var animator: PolygonAnimator? = null
-
-    override fun onGamesAround(games: List<Feature>) = activity.runOnUiThread {
+    fun showFeatures(games: List<Feature>) = runOnUiThread finish@ {
+        val map = map ?: return@finish
         OSMShortcuts.refreshGeojsonFeaturesOnMap(map, games.map { GeoJsonPolygon(it.id, it.geojson) })
     }
 
-    override fun onGameStarted(info: GameInfo) = activity.runOnUiThread {
-        this.info = info
-        this.animator = OSMShortcuts.animatePolygonOverlay(map, info.game)
+    fun startGame(info: GameInfo) = runOnUiThread finish@ {
+        val map = map ?: return@finish
+        animator = OSMShortcuts.animatePolygonOverlay(map, info.game)
         animator?.overlay?.let { OSMShortcuts.focus(map, it.boundingBox.center) }
-        TransparentDialog(activity, "Game ${info.game} started.\nYour role is: ${info.role}").showWithTimeout(dialogsDelay)
+
+        val app = application as CatchCatch
+        game = GameEventHandler(app.socket!!, info, this)
+        radar!!.switchTo(game!!)
     }
 
-    override fun onGameTargetNear(meters: Double) = activity.runOnUiThread {
-        OSMShortcuts.drawCircleOnMap(map, "target-dist", activity.player.point(), meters, 1000.0)
+    fun showCircleAroundPlayer(meters: Double) = runOnUiThread finish@ {
+        val map = map ?: return@finish
+        OSMShortcuts.drawCircleOnMap(map, "player-circle", player.point(), meters, 1000.0)
     }
 
-    override fun onGameTargetReached(meters: Double) = activity.runOnUiThread {
-        TransparentDialog(activity, "You win!\nTarget was ${meters.toInt()}m closer").showWithTimeout(dialogsDelay)
-    }
-
-    override fun onGameTargetWin() = activity.runOnUiThread {
-        TransparentDialog(activity, "Congratulations!\nYou survived").showWithTimeout(dialogsDelay)
-    }
-
-    override fun onGameLoose(gameID: String) = activity.runOnUiThread {
-        finish()
-        TransparentDialog(activity, "You loose =/").showWithTimeout(dialogsDelay)
-    }
-
-    override fun onGameFinish(rank: GameRank) = activity.runOnUiThread {
-        finish()
-        Handler().postDelayed({
-            RankDialog(activity, rank).showWithTimeout(dialogsDelay)
-        }, 3000)
-    }
-
-    override fun onDisconnected() {
+    fun gameOver() = runOnUiThread {
         animator?.stop()
+        game!!.switchTo(radar!!)
     }
 
-    private fun finish() {
-        info = null
-        animator?.stop()
+    fun sendPosition(l: Location) {
+        val sock = (application as CatchCatch).socket!!
+        val coords = JSONObject(mapOf("lat" to l.latitude, "lon" to l.longitude))
+        sock.emit("player:update", coords.toString())
     }
 }
