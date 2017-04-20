aa9ff6978f130c341c801b825740735893192233
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-04-20 07:54:52 -0300

    refactor: move all dialogs to Dialogs.kt

--- android/app/src/main/java/io/perenecabuto/catchcatch/Dialogs.kt
+++ android/app/src/main/java/io/perenecabuto/catchcatch/Dialogs.kt
@@ -7,6 +7,40 @@ import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 
+open class BaseDialog(context: android.content.Context) : android.app.Dialog(context) {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.attributes.windowAnimations = io.perenecabuto.catchcatch.R.style.PopUpDialog
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+    }
+
+    override fun show() {
+        try {
+            super.show()
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            return
+        }
+    }
+
+    fun showWithTimeout(millis: Long) {
+        show()
+        android.os.Handler().postDelayed(this::dismiss, millis)
+    }
+}
+
+class TransparentDialog(context: Context, val msg: String) : BaseDialog(context) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.attributes.windowAnimations = R.style.PopUpDialog
+        window.setBackgroundDrawableResource(android.R.color.transparent)
+        setContentView(R.layout.dialog_transparent)
+
+        val container = findViewById(R.id.dialog_transparent_text) as TextView
+        container.text = msg
+    }
+}
+
 class RankDialog(context: Context, val rank: GameRank) : BaseDialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
