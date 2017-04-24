4a697247a1ed7518edb8f5113e3153ddc595a2ed
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-04-23 22:47:18 -0300

    refactor: use simpler arc

--- android/app/src/main/java/io/perenecabuto/catchcatch/RadarView.kt
+++ android/app/src/main/java/io/perenecabuto/catchcatch/RadarView.kt
@@ -4,55 +4,46 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.os.Handler
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
+import java.lang.Math.abs
 
 
 class RadarView(context: Context, val attrs: AttributeSet?) : View(context, attrs) {
-    val paint = Paint().let { it.style = Paint.Style.STROKE; it }
-    val drawHandler: Handler = Handler()
+    val paint = Paint().let {
+        it.style = Paint.Style.FILL
+        it.color = Color.argb(32, 16, 127, 32)
+        it.isAntiAlias = true
+        it.strokeWidth = 3f
+        it
+    }
 
-    val steps = 0.5
-    var angle = 0.0
+    val steps = 0.6f
+    var angle = 0.0f
     var r: Float = 0.0f
     var center: List<Float> = listOf(0f, 0f)
-    val lastPoint = mutableListOf(0f, 0f)
+    var topOffset: Float = 0.0f
 
-    init {
-        autoUpdate()
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, right)
     }
 
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        Log.d("radar center", center.toString() + ":" + r)
-    }
-
-    private fun autoUpdate() {
-        angle = if (angle >= 360) 0.0 else angle + steps
-        lastPoint[0] = center[0] + (r * Math.cos(Math.toRadians(angle))).toFloat()
-        lastPoint[1] = center[1] + (r * Math.sin(Math.toRadians(angle))).toFloat()
-        invalidate()
-        drawHandler.postDelayed(this::autoUpdate, 100)
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        r = (w / 2).toFloat()
+        center = listOf((w / 2).toFloat(), (h / 2).toFloat())
+        topOffset = abs(height/2 - width/2).toFloat()
     }
 
     override fun onDraw(_canvas: Canvas?) {
         val canvas = _canvas ?: return
         super.onDraw(canvas)
-        setupSizes()
-        canvas.drawColor(Color.TRANSPARENT)
-        canvas.drawCircle(center[0], center[1], r / 3, paint)
-        canvas.drawCircle(center[0], center[1], r / 2, paint)
-        canvas.drawCircle(center[0], center[1], r / 1.5f, paint)
-        canvas.drawCircle(center[0], center[1], r, paint)
-        canvas.drawLine(center[0], center[1], lastPoint[0], lastPoint[1], paint)
-    }
+        angle = if (angle >= 360) 0.0f else angle + steps
 
-    private fun setupSizes() {
-        if (r == 0f) {
-            r = (width / 2).toFloat()
-            center = listOf((width / 2).toFloat(), (height / 2).toFloat())
-        }
+        canvas.drawColor(Color.TRANSPARENT)
+        canvas.drawCircle(center[0], center[1], r, paint)
+        canvas.drawArc(0f, topOffset, width.toFloat(), width + topOffset, angle, 60f, true, paint)
+
+        if (!isInEditMode) invalidate()
     }
 }
