991b47cdf15812a707992c5ca1949ab8a6705527
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-05-16 21:13:48 -0300

    refactor: add voice to CatchCatch app

--- android/app/src/main/java/io/perenecabuto/catchcatch/CatchCatch.kt
+++ android/app/src/main/java/io/perenecabuto/catchcatch/CatchCatch.kt
@@ -1,14 +1,17 @@
 package io.perenecabuto.catchcatch
 
 import android.app.Application
+import io.perenecabuto.catchcatch.drivers.GameVoice
 import io.perenecabuto.catchcatch.drivers.WebSocketClient
 
 
 class CatchCatch : Application() {
     private var address = "https://beta-catchcatch.ddns.net/ws"
     internal var socket: WebSocketClient = WebSocketClient(address)
+    internal var tts: GameVoice? = null
 
     override fun onCreate() {
+        tts = GameVoice(this, { tts?.speak("Welcome to CatchCatch.") })
         socket.onDisconnect({ socket.connect() })
         socket.connect()
     }
