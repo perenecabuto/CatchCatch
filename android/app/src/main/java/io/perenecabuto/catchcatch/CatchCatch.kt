e91c3ce1321cd685c026cb5bb66d4c294653c12b
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-05-22 01:01:05 -0300

    feat: add remote addresses
    - add connectTo method
    - add hardcoded server addresses
    - persist selected address on shared preferences

--- android/app/src/main/java/io/perenecabuto/catchcatch/CatchCatch.kt
+++ android/app/src/main/java/io/perenecabuto/catchcatch/CatchCatch.kt
@@ -1,23 +1,41 @@
 package io.perenecabuto.catchcatch
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import io.perenecabuto.catchcatch.drivers.GameVoice
 import io.perenecabuto.catchcatch.drivers.WebSocketClient
 
 
 class CatchCatch : Application() {
-    private var address = "https://beta-catchcatch.ddns.net/ws"
-    internal var socket: WebSocketClient = WebSocketClient(address)
+    companion object {
+        val serverAddresses = listOf(
+            "https://beta-catchcatch.ddns.net",
+            "wss://catchcatch.ddns.net",
+            "wss://catchcatch.pointto.us"
+        )
+    }
+
+    private val prefs: SharedPreferences by lazy { getSharedPreferences(javaClass.simpleName, Context.MODE_PRIVATE) }
+    internal var address
+        get() = prefs.getString("address", serverAddresses[0])
+        set(value) = prefs.edit().putString("address", value).apply()
+
+    internal var socket: WebSocketClient = WebSocketClient()
     internal var tts: GameVoice? = null
 
     override fun onCreate() {
         tts = GameVoice(this, { tts?.speak("Welcome to CatchCatch.") })
-        socket.onDisconnect({ socket.connect() })
-        socket.connect()
+        connectTo(address)
     }
 
     override fun onTerminate() {
         socket.shutdown()
         super.onTerminate()
+    }
+
+    internal fun connectTo(addr: String) {
+        address = addr
+        socket.connect(addr + "/ws")
     }
 }
