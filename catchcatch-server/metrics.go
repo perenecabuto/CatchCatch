80baee9c6e313f8780ff89f2b870bfeb42820875
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-09-13 22:10:27 -0300

    feat: add global metrics collector

--- catchcatch-server/metrics.go
+++ catchcatch-server/metrics.go
@@ -3,7 +3,10 @@ package main
 import (
 	"time"
 
+	_ "github.com/tevjef/go-runtime-metrics/expvar"
+
 	influxdb "github.com/influxdata/influxdb/client/v2"
+	metrics "github.com/tevjef/go-runtime-metrics"
 )
 
 var MetricsTimeout = time.Second
@@ -12,6 +15,7 @@ type Tags map[string]string
 type Values map[string]interface{}
 
 type MetricsCollector struct {
+	addr     string
 	db       string
 	username string
 	password string
@@ -25,7 +29,7 @@ func NewMetricsCollector(addr, db, username, password string) (*MetricsCollector
 		Password: password,
 		Timeout:  MetricsTimeout,
 	})
-	return &MetricsCollector{db, username, password, client}, err
+	return &MetricsCollector{addr, db, username, password, client}, err
 }
 
 func (c MetricsCollector) Ping() error {
@@ -48,4 +52,8 @@ func (c MetricsCollector) Notify(measurement string, tags Tags, values Values) e
 	}
 	bp.AddPoint(point)
 	return c.client.Write(bp)
+}
+
+func (c MetricsCollector) RunGlobalCollector() error {
+	return metrics.RunCollector(&metrics.Config{Database: c.db, Host: c.addr})
 }
