77bf7a4efb7f46dc233eba33c284bfe8ab9b4004
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-11-21 22:52:39 -0200

    refactor: move checkpoint watcher to its own file

--- catchcatch-server/gamewatcher.go
+++ catchcatch-server/gamewatcher.go
@@ -149,30 +149,6 @@ func (gw *GameWatcher) startGameWhenReady(ctx context.Context, g *Game) error {
 	}
 }
 
-// WatchCheckpoints ...
-func (gw *GameWatcher) WatchCheckpoints(ctx context.Context) {
-	err := gw.stream.StreamNearByEvents(ctx, "player", "checkpoint", "*", 1000, func(d *Detection) error {
-		if d.Intersects == Exit {
-			return nil
-		}
-
-		payload := &protobuf.Detection{
-			EventName:    proto.String("checkpoint:detected"),
-			Id:           &d.FeatID,
-			FeatId:       &d.FeatID,
-			Lon:          &d.Lon,
-			Lat:          &d.Lat,
-			NearByFeatId: &d.NearByFeatID,
-			NearByMeters: &d.NearByMeters,
-		}
-		return gw.wss.Emit(d.FeatID, payload)
-	})
-	if err != nil {
-		log.Println("Error to stream geofence:event", err)
-		debug.PrintStack()
-	}
-}
-
 // game callbacks
 
 // OnGameStarted implements GameEvent.OnGameStarted
