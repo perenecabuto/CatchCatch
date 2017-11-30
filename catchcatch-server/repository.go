c17a7eedceb4b76178609ba0734b9ff8aa33b932
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-11-30 01:12:06 -0200

    refactor: extract tile38 repository and use it by composition

---
+++ catchcatch-server/repository.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"github.com/perenecabuto/CatchCatch/catchcatch-server/model"
+
+	geo "github.com/kellydunn/golang-geo"
+	redis "gopkg.in/redis.v5"
+)
+
+var (
+	// ErrFeatureNotFound happens when feature does not exists in the storage
+	ErrFeatureNotFound = redis.Nil
+)
+
+type Repository interface {
+	SetFeature(group, id, geojson string) (*model.Feature, error)
+	Exists(group, id string) (bool, error)
+	FeatureByID(group, id string) (*model.Feature, error)
+	RemoveFeature(group, id string) error
+	Features(group string) ([]*model.Feature, error)
+	FeaturesAround(group string, point *geo.Point) ([]*model.Feature, error)
+	Clear() error
+}
+
+type Tile38Repository struct {
+	client *redis.Client
+}
+
+func NewRepository(client *redis.Client) Repository {
+	return &Tile38Repository{client}
+}
+
+// SetFeature persist feature
+func (r *Tile38Repository) SetFeature(group, id, geojson string) (*model.Feature, error) {
+	cmd := redis.NewStringCmd("SET", group, id, "OBJECT", geojson)
+	r.client.Process(cmd)
+	if err := cmd.Err(); err != nil {
+		return nil, err
+	}
+	return &model.Feature{ID: id, Coordinates: geojson, Group: group}, nil
+}
+
+// RemoveFeature removes a feature
+func (r *Tile38Repository) RemoveFeature(group, id string) error {
+	cmd := redis.NewStringCmd("DEL", group, id)
+	r.client.Process(cmd)
+	return cmd.Err()
+}
+
+// Features ...
+func (r *Tile38Repository) Features(group string) ([]*model.Feature, error) {
+	cmd := redis.NewSliceCmd("SCAN", group)
+	return featuresFromSliceCmd(r.client, group, cmd)
+}
+
+// Exists ...
+func (r *Tile38Repository) Exists(group, id string) (bool, error) {
+	f, err := r.FeatureByID(group, id)
+	if err == redis.Nil {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return f != nil, nil
+}
+
+// FeatureByID ...
+func (r *Tile38Repository) FeatureByID(group, id string) (*model.Feature, error) {
+	cmd := redis.NewStringCmd("GET", group, id)
+	r.client.Process(cmd)
+	coords, err := cmd.Result()
+	if err != nil {
+		return nil, err
+	}
+	return &model.Feature{ID: id, Group: group, Coordinates: coords}, nil
+}
+
+// FeaturesAround return feature group near by point
+func (r *Tile38Repository) FeaturesAround(group string, point *geo.Point) ([]*model.Feature, error) {
+	dist := 1000
+	cmd := redis.NewSliceCmd("NEARBY", group, "POINT", point.Lat(), point.Lng(), dist)
+	return featuresFromSliceCmd(r.client, group, cmd)
+}
+
+// Clear the database
+func (r *Tile38Repository) Clear() error {
+	return r.client.FlushDb().Err()
+}
+
+func featuresFromSliceCmd(client *redis.Client, group string, cmd *redis.SliceCmd) ([]*model.Feature, error) {
+	client.Process(cmd)
+	res, err := cmd.Result()
+	if err != nil {
+		return nil, err
+	}
+	payload, _ := redis.NewSliceResult(res[1].([]interface{}), err).Result()
+	features := make([]*model.Feature, len(payload))
+	for i, item := range payload {
+		itemRes, _ := redis.NewSliceResult(item.([]interface{}), nil).Result()
+		features[i] = &model.Feature{ID: itemRes[0].(string), Group: group, Coordinates: itemRes[1].(string)}
+	}
+	return features, nil
+}
--- catchcatch-server/service.go
+++ catchcatch-server/service.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	geo "github.com/kellydunn/golang-geo"
+	"fmt"
+
 	"github.com/perenecabuto/CatchCatch/catchcatch-server/model"
 	gjson "github.com/tidwall/gjson"
-	redis "gopkg.in/redis.v5"
-)
-
-var (
-	// ErrFeatureNotFound happens when feature does not exists in the storage
-	ErrFeatureNotFound = redis.Nil
 )
 
 // PlayerLocationService manage players and features
@@ -18,23 +13,21 @@ type PlayerLocationService interface {
 	Update(p *model.Player) error
 	Remove(p *model.Player) error
 	Players() (model.PlayerList, error)
-
-	SetFeature(group, id, geojson string) (*model.Feature, error)
-	FeatureByID(group, id string) (*model.Feature, error)
-	Features(group string) ([]*model.Feature, error)
-	FeaturesAround(group string, point *geo.Point) ([]*model.Feature, error)
-
-	Clear()
 }
 
 // Tile38PlayerLocationService manages player locations
 type Tile38PlayerLocationService struct {
-	client *redis.Client
+	repo Repository
 }
 
 // NewPlayerLocationService build a PlayerLocationService
-func NewPlayerLocationService(client *redis.Client) PlayerLocationService {
-	return &Tile38PlayerLocationService{client}
+func NewPlayerLocationService(repo Repository) PlayerLocationService {
+	return &Tile38PlayerLocationService{repo}
+}
+
+// Register add new player
+func (s *Tile38PlayerLocationService) Exists(p *model.Player) (bool, error) {
+	return s.repo.Exists("player", p.ID)
 }
 
 // Register add new player
@@ -44,21 +37,19 @@ func (s *Tile38PlayerLocationService) Register(p *model.Player) error {
 
 // Update player data
 func (s *Tile38PlayerLocationService) Update(p *model.Player) error {
-	cmd := redis.NewStringCmd("SET", "player", p.ID, "POINT", p.Lat, p.Lon)
-	s.client.Process(cmd)
-	return cmd.Err()
+	_, err := s.repo.SetFeature("player", p.ID,
+		fmt.Sprintf(`{"type": "Point", "coordinates": [%f, %f]}`, p.Lon, p.Lat))
+	return err
 }
 
 // Remove player
 func (s *Tile38PlayerLocationService) Remove(p *model.Player) error {
-	cmd := redis.NewStringCmd("DEL", "player", p.ID)
-	s.client.Process(cmd)
-	return cmd.Err()
+	return s.repo.RemoveFeature("player", p.ID)
 }
 
 // Players return all registered players
 func (s *Tile38PlayerLocationService) Players() (model.PlayerList, error) {
-	features, err := s.Features("player")
+	features, err := s.repo.Features("player")
 	if err != nil {
 		return nil, err
 	}
@@ -71,70 +62,4 @@ func (s *Tile38PlayerLocationService) Players() (model.PlayerList, error) {
 		list[i] = &model.Player{ID: f.ID, Lat: coords[1].Float(), Lon: coords[0].Float()}
 	}
 	return list, nil
-}
-
-// SetFeature persist features
-func (s *Tile38PlayerLocationService) SetFeature(group, id, geojson string) (*model.Feature, error) {
-	cmd := redis.NewStringCmd("SET", group, id, "OBJECT", geojson)
-	s.client.Process(cmd)
-	if err := cmd.Err(); err != nil {
-		return nil, err
-	}
-	return &model.Feature{ID: id, Coordinates: geojson, Group: group}, nil
-}
-
-// Features ...
-func (s *Tile38PlayerLocationService) Features(group string) ([]*model.Feature, error) {
-	cmd := redis.NewSliceCmd("SCAN", group)
-	return featuresFromSliceCmd(s.client, group, cmd)
-}
-
-// Exists ...
-func (s *Tile38PlayerLocationService) Exists(group, id string) (bool, error) {
-	f, err := s.FeatureByID(group, id)
-	if err == redis.Nil {
-		return false, nil
-	}
-	if err != nil {
-		return false, err
-	}
-	return f != nil, nil
-}
-
-// FeatureByID ...
-func (s *Tile38PlayerLocationService) FeatureByID(group, id string) (*model.Feature, error) {
-	cmd := redis.NewStringCmd("GET", group, id)
-	s.client.Process(cmd)
-	coords, err := cmd.Result()
-	if err != nil {
-		return nil, err
-	}
-	return &model.Feature{ID: id, Group: group, Coordinates: coords}, nil
-}
-
-// FeaturesAround return feature group near by point
-func (s *Tile38PlayerLocationService) FeaturesAround(group string, point *geo.Point) ([]*model.Feature, error) {
-	dist := 1000
-	cmd := redis.NewSliceCmd("NEARBY", group, "POINT", point.Lat(), point.Lng(), dist)
-	return featuresFromSliceCmd(s.client, group, cmd)
-}
-
-// Clear the database
-func (s *Tile38PlayerLocationService) Clear() {
-	s.client.FlushDb()
-}
-
-func featuresFromSliceCmd(client *redis.Client, group string, cmd *redis.SliceCmd) ([]*model.Feature, error) {
-	client.Process(cmd)
-	res, err := cmd.Result()
-	if err != nil {
-		return nil, err
-	}
-	payload, _ := redis.NewSliceResult(res[1].([]interface{}), err).Result()
-	features := make([]*model.Feature, len(payload))
-	for i, item := range payload {
-		itemRes, _ := redis.NewSliceResult(item.([]interface{}), nil).Result()
-		features[i] = &model.Feature{ID: itemRes[0].(string), Group: group, Coordinates: itemRes[1].(string)}
-	}
-	return features, nil
 }
