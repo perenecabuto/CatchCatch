2dc064fb6ecd8fb2c983c0da271190501686b949
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2017-12-01 00:01:36 -0200

    refactor: add admin watcher to user feature service and send ws notifications

---
+++ catchcatch-server/admin_watcher.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/perenecabuto/CatchCatch/catchcatch-server/model"
+	"github.com/perenecabuto/CatchCatch/catchcatch-server/protobuf"
+)
+
+type AdminWatcher struct {
+	service GeoFeatureService
+	wss     *WSServer
+}
+
+func NewAdminWatcher(service GeoFeatureService, wss *WSServer) *AdminWatcher {
+	return &AdminWatcher{service, wss}
+}
+
+// WatchPlayers observe players around players and notify it's position
+func (w *AdminWatcher) WatchPlayers(ctx context.Context) error {
+	return w.service.ObservePlayersAround(ctx, func(playerID string, remotePlayer model.Player, exit bool) error {
+		evtName := proto.String("remote-player:update")
+		if exit {
+			w.wss.Close(remotePlayer.ID)
+			evtName = proto.String("remote-player:destroy")
+		}
+		err := w.wss.Emit(playerID, &protobuf.Player{EventName: evtName,
+			Id: &remotePlayer.ID, Lon: &remotePlayer.Lon, Lat: &remotePlayer.Lat})
+		if err != ErrWSConnectionNotFound && err != nil {
+			log.Println("remote-player:updated error", err.Error())
+		}
+		return nil
+	})
+}
+
+// WatchGeofences watch for geofences events and notify players around
+func (w *AdminWatcher) WatchGeofences(ctx context.Context) error {
+	return w.service.ObservePlayerNearToFeature(ctx, "geofences", func(playerID string, distTo float64, f model.Feature) error {
+		err := w.wss.Emit(playerID,
+			&protobuf.Feature{
+				EventName: proto.String("admin:feature:added"), Id: &f.ID,
+				Group: proto.String("geofences"), Coords: &f.Coordinates})
+		if err != nil {
+			log.Println("AdminWatcher:WatchGeofences:", err.Error())
+		}
+		return nil
+	})
+}
+
+// WatchCheckpoints ...
+func (w *AdminWatcher) WatchCheckpoints(ctx context.Context) error {
+	return w.service.ObservePlayerNearToFeature(ctx, "checkpoint", func(playerID string, distTo float64, f model.Feature) error {
+		payload := &protobuf.Detection{
+			EventName:    proto.String("checkpoint:detected"),
+			Id:           &f.ID,
+			FeatId:       &f.ID,
+			NearByFeatId: &playerID,
+			NearByMeters: &distTo,
+		}
+		if err := w.wss.Emit(playerID, payload); err != nil {
+			log.Println("AdminWatcher:WatchCheckpoints:", err.Error())
+		}
+		return nil
+	})
+}
