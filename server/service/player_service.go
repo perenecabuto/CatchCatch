4d58bd55b99758e251c3a96b6ffcfabcca53ed52
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-02-06 00:26:10 -0200

    refactor: move Oberserver methods to PlayerLocationService

--- server/service/player_service.go
+++ server/service/player_service.go
@@ -6,27 +6,31 @@ import (
 
 	"github.com/perenecabuto/CatchCatch/server/model"
 	"github.com/perenecabuto/CatchCatch/server/service/repository"
-
 	gjson "github.com/tidwall/gjson"
 )
-
-const ErrFeatureNotFound = repository.ErrFeatureNotFound
 
 // PlayerLocationService manage players and features
 type PlayerLocationService interface {
 	Set(p *model.Player) error
 	Remove(p *model.Player) error
 	All() (model.PlayerList, error)
+
+	ObservePlayersAround(context.Context, PlayersAroundCallback) error
+	ObservePlayerNearToFeature(context.Context, string, PlayerNearToFeatureCallback) error
 }
+
+type PlayerNearToFeatureCallback func(playerID string, distTo float64, f model.Feature) error
+type PlayersAroundCallback func(playerID string, movingPlayer model.Player, exit bool) error
 
 // Tile38PlayerLocationService manages player locations
 type Tile38PlayerLocationService struct {
-	repo repository.Repository
+	repo   repository.Repository
+	stream repository.EventStream
 }
 
 // NewPlayerLocationService build a PlayerLocationService
-func NewPlayerLocationService(repo repository.Repository) PlayerLocationService {
-	return &Tile38PlayerLocationService{repo}
+func NewPlayerLocationService(repo repository.Repository, stream repository.EventStream) PlayerLocationService {
+	return &Tile38PlayerLocationService{repo, stream}
 }
 
 // Exists add new player
@@ -63,45 +67,7 @@ func (s *Tile38PlayerLocationService) All() (model.PlayerList, error) {
 	return list, nil
 }
 
-type GeoFeatureService interface {
-	FeaturesAroundPlayer(group string, player model.Player) ([]*model.Feature, error)
-	FeaturesByGroup(group string) ([]*model.Feature, error)
-	SetFeature(group, id, geojson string) error
-	Clear() error
-
-	ObservePlayersAround(context.Context, PlayersAroundCallback) error
-	ObservePlayerNearToFeature(context.Context, string, PlayerNearToFeatureCallback) error
-}
-
-type PlayerNearToFeatureCallback func(playerID string, distTo float64, f model.Feature) error
-type PlayersAroundCallback func(playerID string, movingPlayer model.Player, exit bool) error
-
-func NewGeoFeatureService(repo repository.Repository, stream repository.EventStream) GeoFeatureService {
-	return &Tile38GeoFeatureService{repo, stream}
-}
-
-type Tile38GeoFeatureService struct {
-	repo   repository.Repository
-	stream repository.EventStream
-}
-
-func (s *Tile38GeoFeatureService) FeaturesByGroup(group string) ([]*model.Feature, error) {
-	return s.repo.Features(group)
-}
-
-func (s *Tile38GeoFeatureService) FeaturesAroundPlayer(group string, p model.Player) ([]*model.Feature, error) {
-	return s.repo.FeaturesAround(group, p.Point())
-}
-
-func (s *Tile38GeoFeatureService) SetFeature(group, id, geojson string) error {
-	_, err := s.repo.SetFeature(group, id, geojson)
-	return err
-}
-func (s *Tile38GeoFeatureService) Clear() error {
-	return s.repo.Clear()
-}
-
-func (s *Tile38GeoFeatureService) ObservePlayersAround(ctx context.Context, callback PlayersAroundCallback) error {
+func (s *Tile38PlayerLocationService) ObservePlayersAround(ctx context.Context, callback PlayersAroundCallback) error {
 	return s.stream.StreamNearByEvents(ctx, "player", "player", "*", DefaultGeoEventRange, func(d *repository.Detection) error {
 		playerID := d.NearByFeatID
 		movingPlayer := model.Player{ID: d.FeatID, Lon: d.Lon, Lat: d.Lat}
@@ -109,7 +75,7 @@ func (s *Tile38GeoFeatureService) ObservePlayersAround(ctx context.Context, call
 	})
 }
 
-func (s *Tile38GeoFeatureService) ObservePlayerNearToFeature(ctx context.Context, group string, callback PlayerNearToFeatureCallback) error {
+func (s *Tile38PlayerLocationService) ObservePlayerNearToFeature(ctx context.Context, group string, callback PlayerNearToFeatureCallback) error {
 	return s.stream.StreamNearByEvents(ctx, group, "player", "*", DefaultGeoEventRange, func(d *repository.Detection) error {
 		if d.Intersects == repository.Inside {
 			playerID := d.NearByFeatID
