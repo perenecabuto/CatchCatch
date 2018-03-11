3e38dc1707cc6ec26293c1317e1d080d734d2775
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-03-11 11:43:58 -0300

    feat: implement gocraft worker manager

---
+++ server/worker/gocraft.go
@@ -0,0 +1,103 @@
+package worker
+
+import (
+	"log"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gocraft/work"
+)
+
+// GocraftWorkerManager gocraft/work implementation of worker.Manager
+type GocraftWorkerManager struct {
+	pool     *work.WorkerPool
+	enqueuer *work.Enqueuer
+}
+
+// NewGocraftWorkerManager creates a new GocraftWorkerManager
+func NewGocraftWorkerManager(pool *redis.Pool) Manager {
+	ctx := struct{}{}
+	return &GocraftWorkerManager{
+		enqueuer: work.NewEnqueuer("catchcatch", pool),
+		pool:     work.NewWorkerPool(ctx, 1, "catchcatch", pool),
+	}
+}
+
+// Start the worker manager pool
+func (wm *GocraftWorkerManager) Start() {
+	wm.pool.Start()
+}
+
+// Stop the worker manager pool
+func (wm *GocraftWorkerManager) Stop() {
+	wm.pool.Stop()
+}
+
+// Add workers to pool
+func (wm *GocraftWorkerManager) Add(w Worker) {
+	wm.pool.Job(w.ID(), func(job *work.Job) error {
+		lockKey := "lock:" + job.Name
+		locked, err := writeLock(wm.enqueuer.Pool, lockKey, job.ID, 1000)
+		if locked {
+			log.Printf("Job: %s already running", job.Name)
+			return nil
+		}
+		if err != nil {
+			log.Printf("Job: %s error on %s", job.Name, err.Error())
+			return err
+		}
+		defer releaseLock(wm.enqueuer.Pool, lockKey, job.ID)
+
+		return w.Job(job.Args)
+	})
+}
+
+// Run worker to process task
+func (wm *GocraftWorkerManager) Run(w Worker, params map[string]interface{}) error {
+	_, err := wm.enqueuer.Enqueue(w.ID(), params)
+	return err
+}
+
+// writeLock attempts to grab a redis lock. The error returned is safe to ignore
+// if all you care about is whether or not the lock was acquired successfully.
+func writeLock(pool *redis.Pool, name, secret string, ttl uint64) (bool, error) {
+	rc := pool.Get()
+	defer rc.Close()
+	resp, err := rc.Do("SET", name, secret, "PX", ttl, "NX")
+	if err != nil {
+		return false, err
+	}
+	return resp == nil, nil
+}
+
+// writeLock releases the redis lock
+func releaseLock(pool *redis.Pool, name, secret string) (bool, error) {
+	rc := pool.Get()
+	defer rc.Close()
+	script := redis.NewScript(1, unlockScript)
+	resp, err := redis.Int(script.Do(rc, name, secret))
+	if err != nil {
+		return false, err
+	}
+	return resp == 0, nil
+}
+
+const lockScript = `
+local v = redis.call("GET", KEYS[1])
+if v == false or v == ARGV[1]
+then
+	return redis.call("SET", KEYS[1], ARGV[1], "EX", ARGV[2]) and 1
+else
+	return 0
+end
+`
+
+const unlockScript = `
+local v = redis.call("GET",KEYS[1])
+if v == false then
+	return 1
+elseif v == ARGV[1] then
+	return redis.call("DEL",KEYS[1])
+else
+	return 0
+end
+`
---
+++ server/worker/worker_test.go
@@ -0,0 +1,119 @@
+package worker
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"testing"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+	"github.com/gocraft/work"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(m *testing.M) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	go func() {
+		for range signalChan {
+			log.Println("Received an interrupt, stopping services...")
+			os.Exit(0)
+		}
+	}()
+
+	os.Exit(m.Run())
+}
+
+func TestGocraftWorkerManager(t *testing.T) {
+	redisPool1 := &redis.Pool{
+		MaxActive: 5,
+		MaxIdle:   5,
+		Wait:      true,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+	redisPool2 := &redis.Pool{
+		MaxActive: 5,
+		MaxIdle:   5,
+		Wait:      true,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+	redisPool3 := &redis.Pool{
+		MaxActive: 5,
+		MaxIdle:   5,
+		Wait:      true,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+
+	manager1 := NewGocraftWorkerManager(redisPool1)
+	manager2 := NewGocraftWorkerManager(redisPool2)
+	manager3 := NewGocraftWorkerManager(redisPool3)
+
+	manager1.Start()
+	manager2.Start()
+	manager3.Start()
+
+	worker := mockWorker("worker1")
+
+	manager1.Add(worker)
+	manager2.Add(worker)
+	manager3.Add(worker)
+
+	err := manager1.Run(worker, nil)
+	assert.NoError(t, err)
+	err = manager1.Run(worker, nil)
+	assert.NoError(t, err)
+
+	err = manager2.Run(worker, nil)
+	assert.NoError(t, err)
+
+	err = manager2.Run(worker, nil)
+	assert.NoError(t, err)
+
+	err = manager3.Run(worker, nil)
+	assert.NoError(t, err)
+
+	err = manager3.Run(worker, nil)
+	assert.NoError(t, err)
+
+	time.Sleep(time.Second * 10)
+
+	client := work.NewClient("catchcatch", redisPool1)
+	observations, _ := client.WorkerObservations()
+	var busyObservations []*work.WorkerObservation
+	for _, ob := range observations {
+		if ob.IsBusy {
+			busyObservations = append(busyObservations, ob)
+		}
+	}
+
+	go manager1.Stop()
+	go manager2.Stop()
+	go manager3.Stop()
+
+	time.Sleep(time.Second * 10)
+
+	assert.Equal(t, 1, len(busyObservations))
+}
+
+type mockWorker string
+
+func (w mockWorker) ID() string {
+	return string(w)
+}
+
+func (w mockWorker) Job(params map[string]interface{}) error {
+	log.Println("Starting job: ", w.ID())
+	for i := 0; i < 10; i++ {
+		<-time.NewTimer(time.Second).C
+		log.Println("Last:" + time.Now().String())
+	}
+	return nil
+}
