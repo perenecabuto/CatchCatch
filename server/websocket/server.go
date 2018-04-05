8eaf634f96c05713a5b2c3025820237344521ed0
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-04-05 18:12:40 -0300

    refactor: pass WSEventHandler to constructor
    and add method OnConnection and OnStart to it

--- server/websocket/server.go
+++ server/websocket/server.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/golang/protobuf/proto"
 
-	"github.com/perenecabuto/CatchCatch/server/execfunc"
 	"github.com/perenecabuto/CatchCatch/server/protobuf"
 )
 
@@ -25,8 +24,8 @@ type WSDriver interface {
 
 // WSServer manage WS connections
 type WSServer struct {
-	driver       WSDriver
-	OnConnection func(c *WSConnectionHandler)
+	driver  WSDriver
+	handler WSEventHandler
 
 	connections connectionGroup
 	sync.RWMutex
@@ -35,36 +34,43 @@ type WSServer struct {
 type connectionGroup map[string]*WSConnectionHandler
 
 // NewWSServer create a new WSServer
-func NewWSServer(driver WSDriver) *WSServer {
-	wss := &WSServer{driver: driver, OnConnection: func(c *WSConnectionHandler) {}}
+func NewWSServer(driver WSDriver, handler WSEventHandler) *WSServer {
+	wss := &WSServer{driver: driver, handler: handler}
 	wss.connections = make(connectionGroup)
 	return wss
 }
 
 // WSEventHandler is responsible to handle WSConnectionHandler events
 type WSEventHandler interface {
-	OnConnection(c *WSConnectionHandler)
-}
-
-// SetEventHandler set the event handler to new connections
-func (wss *WSServer) SetEventHandler(h WSEventHandler) {
-	if h != nil {
-		wss.OnConnection = h.OnConnection
-	}
+	OnStart(context.Context, *WSServer) error
+	OnConnection(context.Context, *WSConnectionHandler) error
 }
 
 // Listen to WS connections
 func (wss *WSServer) Listen(ctx context.Context) http.Handler {
-	return wss.driver.HTTPHandler(ctx, func(ctx context.Context, c WSConnection) {
+	err := wss.handler.OnStart(ctx, wss)
+	if err != nil {
+		// TODO: return this error
+		log.Panic(err)
+		return nil
+	}
+
+	return wss.driver.HTTPHandler(ctx, func(connctx context.Context, c WSConnection) {
 		conn := wss.Add(c)
-		err := execfunc.WithRecover(func() error {
-			conn.Emit(&protobuf.Simple{EventName: proto.String("connect"), Id: &conn.ID})
-			wss.OnConnection(conn)
-			defer wss.Remove(conn.ID)
-			return conn.listen(ctx)
-		})
+		defer wss.Remove(conn.ID)
+		err := conn.Emit(&protobuf.Simple{EventName: proto.String("connect"), Id: &conn.ID})
 		if err != nil {
-			log.Println("WSServer: read error", err)
+			log.Println("[WSServer] Listen: error to notify connect event:", err)
+			return
+		}
+		err = wss.handler.OnConnection(connctx, conn)
+		if err != nil {
+			log.Println("[WSServer] Listen: handler.OnConnection error:", err)
+			return
+		}
+		err = conn.listen(connctx)
+		if err != nil {
+			log.Println("[WSServer] Listen: conn.listen error:", err)
 		}
 	})
 }
