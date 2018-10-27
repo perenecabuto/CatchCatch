2f12839d3fc1f50d13f1fd653a24aad5a3fef977
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-10-27 12:11:36 -0300

    doc: move todos to todo.go

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -12,23 +12,6 @@ import (
 	"github.com/perenecabuto/CatchCatch/client"
 )
 
-var addr = flag.String("addr", "localhost:5000", "http service address")
-
-// TODO: OnDisconnect
-// TODO: add request
-//// TODO: SERVER validate step size on server when player is in game
-//// TODO: SERVER check rank bug
-//// TODO: SERVER bug disconnect admin
-// TODO: get/request notifications about games around
-// TODO: request game ranking
-// TODO: request global ranking
-// TODO: request features info (game arena, checkpoint)
-// TODO: request how many players are around
-// TODO: bin to load player routes from shape file
-// TODO: admin client
-// TODO: add event to listen for players closer/inside a shape
-// TODO: admin event for player connected
-// TODO: admin event for player entered into a game
 var (
 	addr = flag.String("addr", "localhost:5000", "http service address")
 )
---
+++ client/cmd/todo.go
@@ -0,0 +1,17 @@
+package main
+
+// TODO: OnDisconnect (disconnect errors)
+// TODO: add request
+//// TODO: SERVER validate step size on server when player is in game
+//// TODO: SERVER check rank bug
+//// TODO: SERVER bug disconnect admin
+// TODO: get/request notifications about games around
+// TODO: request game ranking
+// TODO: request global ranking
+// TODO: request features info (game arena, checkpoint)
+// TODO: request how many players are around
+// TODO: bin to load player routes from shape file
+// TODO: admin client
+// TODO: add event to listen for players closer/inside a shape
+// TODO: admin event for player connected
+// TODO: admin event for player entered into a game
