d77fc621809cde64810ecbb4ff0083b623e0ce84
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-11-21 13:25:10 -0200

    doc: add description to PlayerHandler events

--- server/core/player_handler.go
+++ server/core/player_handler.go
@@ -13,6 +13,7 @@ import (
 	"github.com/perenecabuto/CatchCatch/server/websocket"
 )
 
+// PlayerHandler events
 const (
 	EventPlayerRegistered = "player:registered"
 	EventPlayerDisconnect = "player:disconnect"
