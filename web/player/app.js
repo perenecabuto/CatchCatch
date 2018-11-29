0a399a716906276d2f4895f85be0a90897ab39ce
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-11-29 19:13:43 -0200

    feat: add web player base

---
+++ web/player/app.js
@@ -0,0 +1,35 @@
+const init = () => {
+    const go = new Go();
+    const addr = "localhost:5000";
+    let mod, inst;
+
+    WebAssembly.instantiateStreaming(fetch("catchcatch.wasm"), go.importObject).then(async (result) => {
+        mod = result.module;
+        inst = result.instance;
+        await go.run(inst);
+    });
+
+    document.addEventListener("catchcatch:ready", function() {
+        catchcatch.NewPlayer(addr, function(player) {
+            let options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
+
+            const updatePosition = pos => {
+                let lon = pos.coords.longitude;
+                let lat = pos.coords.latitude;
+                player.update(lat, lon);
+            };
+
+            player.onRegistered(function(p) {
+                navigator.geolocation.watchPosition(updatePosition, null, options);
+                navigator.geolocation.getCurrentPosition(updatePosition);
+                player.update(10, 10);
+            });
+        });
+    });
+}
+
+init();
