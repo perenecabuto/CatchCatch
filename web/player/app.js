064210389e1c25557a4acbd7a0011d5d727dc385
Felipe Ramos (perenecabuto) <perenecabuto@gmail.com>
2018-11-30 04:11:46 -0200

    feat: add map to player page

--- web/player/app.js
+++ web/player/app.js
@@ -9,6 +9,17 @@ const init = () => {
         await go.run(inst);
     });
 
+    let mapSource = new ol.source.Stamen({layer:"toner"});
+    let raster = new ol.layer.Tile({ source: mapSource });
+    let source = new ol.source.Vector({ wrapX: false });
+    let vector = new ol.layer.Vector({ source: source });
+    let view = new ol.View({ center: [0, 0], zoom: 17, projection: "EPSG:4326" })
+    let map = new ol.Map({
+        interactions: ol.interaction.defaults({ mouseWheelZoom: false }),
+        controls: [],
+        layers: [raster, vector], target: 'map', view: view
+    });
+
     document.addEventListener("catchcatch:ready", function() {
         catchcatch.NewPlayer(addr, function(player) {
             let options = {
@@ -21,13 +32,18 @@ const init = () => {
                 let lon = pos.coords.longitude;
                 let lat = pos.coords.latitude;
                 player.update(lat, lon);
+                view.setCenter([lon, lat]);
             };
 
             player.onRegistered(function(p) {
-                console.log(p)
+                console.log(p);
                 navigator.geolocation.watchPosition(updatePosition, null, options);
                 navigator.geolocation.getCurrentPosition(updatePosition);
-                player.update(10, 10);
+            });
+
+            map.on('moveend', function(evt) {
+                let center = map.getView().getCenter();
+                player.update(center[1], center[0]);
             });
         });
     });
